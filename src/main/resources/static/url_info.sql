3c08d0de22a16ab82225bb8a0e1f078dca9baf8f
beichenhpy <beichenhpy@outlook.com>
2021-03-03 09:38:17 +0800

    1、修改mysql->pgsql 2、添加sql文件

---
+++ src/main/resources/static/url_info.sql
@@ -0,0 +1,15 @@
+create table "short-url".url_info
+(
+    id         bigserial not null
+        constraint "url-info_pk"
+        primary key,
+    short_url  varchar(16),
+    origin_url varchar(100)
+);
+
+comment on column "short-url".url_info.short_url is '短url';
+
+comment on column "short-url".url_info.origin_url is '原始url';
+
+alter table "short-url".url_info
+    owner to beichen;
