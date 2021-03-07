1a6a0e1c837464742c6609ee7b85fe0ff6a807b2
hanpengyu <beichenhpy@outlook.com>
2021-03-07 15:05:56 +0800

    一些提交 1、test去除

--- src/test/java/cn/beichenhpy/shorturl/ShortUrlApplicationTests.java
+++ src/test/java/cn/beichenhpy/shorturl/ShortUrlApplicationTests.java
@@ -14,8 +14,7 @@ class ShortUrlApplicationTests {
 
     @Test
     void contextLoads() {
-        String su = defaultShortUrlServiceImpl.addUrlInfo("https://bilibili.com/?from=bill_top_mnav&spm_id_from=888.29530.b_696e7465726e6174696f6e616c486561646572.6");
-        System.out.println(su);
+
     }
 
 }
