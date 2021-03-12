7e5acf90b8e56f344617a6c636839c2bda45933c
hanpengyu <beichenhpy@outlook.com>
2021-03-12 19:39:58 +0800

    日志log切面添加对bindingResult的排除

--- src/main/java/cn/beichenhpy/shorturl/aop/SysLogAspect.java
+++ src/main/java/cn/beichenhpy/shorturl/aop/SysLogAspect.java
@@ -14,6 +14,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
@@ -100,12 +101,14 @@ public class SysLogAspect {
         //放在中间变量中
         Object[] arguments  = new Object[paramsArray.length];
         for (int i = 0; i < paramsArray.length; i++) {
-            if (paramsArray[i] instanceof ServletRequest || paramsArray[i] instanceof ServletResponse || paramsArray[i] instanceof MultipartFile) {
+            Object param = paramsArray[i];
+            if (param instanceof ServletRequest || param instanceof ServletResponse || param instanceof MultipartFile || param instanceof BindingResult) {
                 //ServletRequest不能序列化，从入参里排除，否则报异常：java.lang.IllegalStateException: It is illegal to call this method if the current request is not in asynchronous mode (i.e. isAsyncStarted() returns false)
                 //ServletResponse不能序列化 从入参里排除，否则报异常：java.lang.IllegalStateException: getOutputStream() has already been called for this response
+                // BindingResult在 fastJson >1.2.75 会报错
                 continue;
             }
-            arguments[i] = paramsArray[i];
+            arguments[i] = param;
         }
         //update-begin-author:taoyan date:20200724 for:日志数据太长的直接过滤掉
         PropertyFilter profiler = (o, name, value) -> value == null || value.toString().length() <= 500;
